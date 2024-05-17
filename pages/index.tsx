d7e0a4a8f27de9108499938dd5d25d24ee5de0c5
CallMeSama <diarratoulaye07@gmail.com>
2024-05-17 10:05:51 +0000

    erreur

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,14 @@ const IndexPage = () => {
     <div className="container">
       <h1 className="text-center mt-5">My Next App</h1>
       <div className="d-flex flex-column align-items-center">
-        <Link href="/about">
-          <a className="btn btn-primary mt-3">About</a>
+        <Link className="btn btn-primary mt-3" href="/about">
+          About
         </Link>
-        <Link href="/movies">
-          <a className="btn btn-primary mt-3">Movies</a>
+        <Link className="btn btn-primary mt-3" href="/movies">
+          Movies
         </Link>
-        <Link href="/product">
-          <a className="btn btn-primary mt-3">Product</a>
+        <Link className="btn btn-primary mt-3" href="/product">
+          Product
         </Link>
       </div>
     </div>
